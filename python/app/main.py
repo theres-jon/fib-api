f7a047ea2ab762212fed4283571c27dc36f3d957
Jonathan Hickman <jonproductions@gmail.com>
2026-01-08 18:13:12 -0500

    chore: running linter

--- python/app/main.py
+++ python/app/main.py
@@ -10,6 +10,7 @@ FIB_MAX = os.getenv("MAX_FIB", "100000")
 
 FIB_MAX_INT = int(FIB_MAX)
 
+
 class GetFibs(BaseHTTPRequestHandler):
 
     def respond(self, status: int, body: str):
@@ -33,10 +34,7 @@ class GetFibs(BaseHTTPRequestHandler):
             return self.respond(422, "'n' must be an integer")
 
         if n < 0 or n > FIB_MAX_INT:
-            return self.respond(
-                422,
-                f"'n' must be between 0 and {FIB_MAX_INT}"
-            )
+            return self.respond(422, f"'n' must be between 0 and {FIB_MAX_INT}")
 
         nums = fibonacci(n)
         result = ", ".join(map(str, nums))
@@ -61,6 +59,7 @@ class GetFibs(BaseHTTPRequestHandler):
             return self.handle_fibonacci(params)
 
         return self.respond(404, "Not Found")
+
 
 if __name__ == "__main__":
     from http.server import HTTPServer
